da6164003e36ee9726f1dcbfca1d3230808d5147
Keyaku <antonio.luis.sarmento@gmail.com>
2017-11-01 16:26:25 +0000

    Updated test.sh to reflect the change of arguments

--- proj/test.sh
+++ proj/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-time ./heatSim 1024 10 10 0 0 100 1 > single.out
-time ./heatSim 1024 10 10 0 0 100 1 32 0 > multi.out
+time ./heatSim 1024 10 10 0 0 100 > single.out
+time ./heatSim 1024 10 10 0 0 100 32 0 > multi.out
 
 diff single.out multi.out > result.diff
 if [ $? -ne 0 ]; then
