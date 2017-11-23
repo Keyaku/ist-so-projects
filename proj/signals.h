804fc892f4de943847c3c4da948fd09ae3dae0ee
Keyaku <antonio.luis.sarmento@gmail.com>
2017-11-23 17:21:43 +0000

    Added joke CFLAGS and fixed warnings

--- proj/signals.h
+++ proj/signals.h
@@ -2,9 +2,9 @@
 #define SIGNALS_H
 
 
-void signals_init();
-int signals_was_interrupted();
-int signals_was_alarmed();
-void signals_reset_alarm();
+void signals_init(int interval);
+int signals_was_interrupted(void);
+int signals_was_alarmed(void);
+void signals_reset_alarm(int interval);
 
 #endif // SIGNALS_H
